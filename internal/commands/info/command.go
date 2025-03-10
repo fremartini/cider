7a672a57b9ab493c11d5868294d149e3a9ef215e
Frederik Martini <fma@cvation.com>
2025-03-10 14:43:18 +0100

    ensure ordering

--- internal/commands/info/command.go
+++ internal/commands/info/command.go
@@ -14,6 +14,10 @@ func New() *handler {
 	return &handler{}
 }
 
+type pair struct {
+	a, b string
+}
+
 func (h *handler) Handle(args []string) error {
 	if len(args) != 1 {
 		return fmt.Errorf("command expects exactly one argument")
@@ -23,29 +27,31 @@ func (h *handler) Handle(args []string) error {
 
 	block := cidr.NewBlock(ip)
 
-	entries := map[string]any{
-		"Address range":       fmt.Sprintf("%s - %s", block.NetworkAddress(), block.BroadcastAddress()),
-		"Start of next block": block.StartAddressOfNextBlock(),
-		"Cidr":                fmt.Sprintf("/%v", block.HostPortion),
-		"Subnet mask":         block.SubnetMask(),
-		"Addresses":           block.AvailableHosts(),
-		"Azure addresses":     block.AvailableAzureHosts(),
+	entries := []pair{
+		{a: "Address range", b: fmt.Sprintf("%s - %s", block.NetworkAddress(), block.BroadcastAddress())},
+		{a: "Start of next block", b: block.StartAddressOfNextBlock()},
+		{a: "Cidr", b: fmt.Sprintf("/%v", block.HostPortion)},
+		{a: "Subnet mask", b: block.SubnetMask()},
+		{a: "Addresses", b: fmt.Sprintf("%v", block.AvailableHosts())},
+		{a: "Azure addresses", b: block.AvailableAzureHosts()},
 	}
 
-	keys := []string{}
-	for k := range entries {
-		keys = append(keys, k)
-	}
-
-	widestTitle := list.Fold(keys, 0, func(e string, acc int) int {
-		m := int(math.Max(float64(acc), float64(len(e))))
-
-		return m
-	})
-
-	for k, v := range entries {
-		fmt.Printf("%s : %v\n", utils.PadRight(k, ' ', widestTitle), v)
-	}
+	printOutput(entries)
 
 	return nil
+}
+
+func printOutput(entries []pair) {
+	keys := []string{}
+	for _, pair := range entries {
+		keys = append(keys, pair.a)
+	}
+
+	longestTitle := list.Fold(keys, 0, func(title string, acc int) int {
+		return int(math.Max(float64(acc), float64(len(title))))
+	})
+
+	for _, pair := range entries {
+		fmt.Printf("%s : %v\n", utils.PadRight(pair.a, ' ', longestTitle), pair.b)
+	}
 }
