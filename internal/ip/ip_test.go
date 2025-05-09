33c3b8ea786ff15d11e2f90960c9b73568485b54
Frederik Martini <fma@cvation.com>
2025-05-09 15:07:27 +0200

    ip test

--- internal/ip/ip_test.go
+++ internal/ip/ip_test.go
@@ -1,1 +1,27 @@
 package ip_test
+
+import (
+	"cider/internal/ip"
+	"testing"
+)
+
+func Test_ToDecimal(t *testing.T) {
+	tests := map[string]struct {
+		ip       *ip.Ip
+		expected int
+	}{
+		"decimal": {ip: ip.NewIp("10.0.0.5"), expected: 167772165},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			actual := test.ip.ToDecimal()
+
+			if actual != test.expected {
+				t.Fatalf("%s: got %v expected %v", name, actual, test.expected)
+			}
+		})
+	}
+}
